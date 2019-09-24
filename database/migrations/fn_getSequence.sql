4333c9bec5e263b49a4b836ef2fdcac4d042d506
mehuliate <darksideofbotix@gmail.com>
2019-09-24 17:13:03 +0700

    update getSequenceSQL

--- database/migrations/fn_getSequence.sql
+++ database/migrations/fn_getSequence.sql
@@ -1,6 +1,6 @@
 CREATE FUNCTION `getSequence`(
-	`kode_sequence` VARCHAR(32),
-	`tahun` INT
+	sKode_sequence VARCHAR(32),
+	sTahun INT
 )
 RETURNS INT(11)
 LANGUAGE SQL
@@ -9,14 +9,14 @@ CONTAINS SQL
 SQL SECURITY DEFINER
 COMMENT ''
 BEGIN
- DECLARE NO INT;
- SET no = (SELECT sequence FROM sequence WHERE kode_sequence = `kode_sequence` AND tahun = `tahun`);
+ DECLARE no INT;
+ SET no = (SELECT sequence FROM sequence WHERE kode_sequence = sKode_sequence AND tahun = sTahun);
  IF no IS NULL THEN
   SET no = 1;
-  INSERT INTO sequence(kode_sequence, tahun, sequence) VALUES(`kode_sequence`, `tahun`, no);
+  INSERT INTO sequence(kode_sequence, tahun, sequence) VALUES(sKode_sequence, sTahun, no);
  ELSE
   SET no = no + 1;
-  UPDATE sequence SET sequence.sequence = no WHERE kode_sequence = `kode_sequence` AND tahun = `tahun`;
+  UPDATE sequence SET sequence.sequence = no WHERE kode_sequence = sKode_sequence AND tahun = sTahun;
  END IF;
  RETURN no;
 END
