a83663c1f4673bba1bab4e863c13df29fb8d3327
Bow Vernon <bowvernon110391@gmail.com>
2019-09-25 08:27:06 +0700

    Add table sspcp_header,sspcp_detail

---
+++ database/migrations/2019_09_25_010901_create_sspcp_header_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateSspcpHeaderTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('sspcp_header', function(Blueprint $table)
+		{
+			$table->integer('id')->unsigned()->primary();
+			$table->integer('cd_header_id')->unsigned()->index('fk_st_header_cd_id_cd_header_id');
+			$table->integer('no_dok')->unsigned();
+			$table->date('tgl_dok');
+			$table->integer('lokasi_id')->unsigned()->index('fk_st_header_lokasi_id_lokasi_id');
+			$table->decimal('total_fob', 18, 4)->nullable();
+			$table->decimal('total_freight', 18, 4)->nullable();
+			$table->decimal('total_insurance', 18, 4)->nullable();
+			$table->decimal('total_cif', 18, 4)->nullable();
+			$table->decimal('total_nilai_pabean', 18, 4)->nullable();
+			$table->decimal('pembebasan', 18, 4)->nullable()->comment('dalam USD');
+			$table->decimal('total_bm', 18, 4)->nullable();
+			$table->decimal('total_ppn', 18, 4)->nullable();
+			$table->decimal('total_ppnbm', 18, 4)->nullable();
+			$table->decimal('total_pph', 18, 4)->nullable();
+			$table->decimal('total_denda', 18, 4)->nullable();
+			$table->text('keterangan', 65535)->nullable();
+			$table->string('kode_valuta', 8)->nullable();
+			$table->string('pemilik_barang')->nullable();
+			$table->string('ground_handler')->nullable();
+			$table->integer('pejabat_id')->unsigned();
+			$table->decimal('nilai_valuta', 18, 4)->nullable();
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('sspcp_header');
+	}
+
+}
