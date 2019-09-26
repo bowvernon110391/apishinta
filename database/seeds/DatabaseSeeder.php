cf85958192865e2fb00948d49f0e94fb3585fd4f
Bow Vernon <bowvernon110391@gmail.com>
2019-09-26 16:45:28 +0700

    Update db seeder to include SPMBSeed as per #2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CDSeeder::class);
         $this->call(SSPCPSeed::class);
         $this->call(ISSeed::class);
+        $this->call(SPMBSeed::class);
     }
 }
