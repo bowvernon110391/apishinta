6d852c0ae58e22b0db490875a4a8d097e2698e6a
mehuliate <darksideofbotix@gmail.com>
2019-10-01 09:50:28 +0700

    add SET GLOBAL log_bin_trust_function_creators =1

--- database/migrations/2019_09_24_022114_create_sequence_function.php
+++ database/migrations/2019_09_24_022114_create_sequence_function.php
@@ -13,6 +13,7 @@ class CreateSequenceFunction extends Migration
      */
     public function up()
     {
+        DB::unprepared('SET GLOBAL log_bin_trust_function_creators =1');
         $queryString = file_get_contents(dirname(__FILE__).'/fn_getSequence.sql');
         DB::unprepared($queryString);
     }
