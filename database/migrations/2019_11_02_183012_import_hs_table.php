0bdf16f4ef146dae7efb965354ec4a0f46563cb7
Bow Vernon <bowvernon110391@gmail.com>
2019-11-02 18:36:22 +0700

    Add hs_code table migration

---
+++ database/migrations/2019_11_02_183012_import_hs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ImportHsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // just read the hs file
+        DB::unprepared(File::get(__DIR__.'./hs_table_dump.sql'));
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('hs_code');
+    }
+}
