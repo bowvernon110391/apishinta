e29ed9dca34a20d0a992424ab31e215aabe41e40
Bow Vernon <bowvernon110391@gmail.com>
2019-11-02 22:45:59 +0700

    Add HsCode model

---
+++ app/HsCode.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HsCode extends Model
+{
+    //
+    public $timestamps = false;
+    protected $table = 'hs_code';
+
+    // Helper
+    static public function storeFullDesc() {
+        // 1st, query all shit
+        echo "Storing full desc for each row...expensive operation here!\n";
+
+        $rows = HsCode::all();
+
+        foreach ($rows as $hs) {
+            echo "Storing data for {$hs->kode}...\n";
+            $hs->full_desc = $hs->deskripsi;
+            $hs->save();
+        }
+
+        echo "Done!";
+    }
+
+    public function scopeByFullDesc($query, $uraian) {
+        return $query->where('full_desc', 'like', "%{$uraian}%");
+    }
+
+    public function scopeByHS($query, $code) {
+        return $query->where('kode', 'like', "{$code}%");
+    }
+
+    public function scopeByJenis($query, $jenis) {
+        return $query->where('jenis_tarif', $jenis);
+    }
+
+    public function scopeByUraian($query, $uraian) {
+        return $query->where('uraian', 'like', "%{$uraian}%");
+    }
+
+    public function scopeByUraianFamily($query, $uraian) {
+        return $query->byUraian($uraian);
+    }
+
+    public function scopeByExactHS($query, $code) {
+        return $query->where('kode', $code);
+    }
+
+    public function scopeByHSHierarchy($query, $code) {
+        // initially
+        $q = $query->byHS($code);
+        // iteratively union
+       
+        while ( ($len = strlen($code)) > 2 ) {
+            $code = substr($code, 0, $len - ($len % 2 ? 1 : 2));
+            $q = $q->orWhere(function ($query) use ($code) {
+                $query->byExactHS($code);
+            });
+        }
+
+        return $q;
+    }
+
+    public function scopeUsable($query) {
+        return $query->whereRaw('LENGTH(`kode`) = 8');
+    }
+
+    // attribute
+    public function getDeskripsiAttribute () {
+        // parent stack
+        $stack = [$this->parent];
+        // text stack
+        $deskripsi = $this->uraian;
+
+        while (count($stack)) {
+            $p = $stack[count($stack)-1];
+            array_pop($stack);
+
+            // check if it's valid
+            if ($p) {
+                $deskripsi = $p->uraian . "\n" . $deskripsi;
+                // push its parent?
+                $stack[] = $p->parent;
+            }
+        }
+        return $deskripsi;
+    }
+
+    // Relations (with self)
+    public function parent() {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    public function children() {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+}
