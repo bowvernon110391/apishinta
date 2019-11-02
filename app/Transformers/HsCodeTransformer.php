96cd15d01e7cbd634f3521838c927732b96eea78
Bow Vernon <bowvernon110391@gmail.com>
2019-11-03 00:57:06 +0700

    Update Hs Code table to add converted tarif ppn + ppnbm

--- app/Transformers/HsCodeTransformer.php
+++ app/Transformers/HsCodeTransformer.php
@@ -14,6 +14,8 @@ class HsCodeTransformer extends TransformerAbstract {
             'uraian'=> $hs->uraian,
             'jenis_tarif' => $hs->jenis_tarif,
             'bm_tarif' => (float) $hs->bm_tarif,
+            'ppn_tarif'=> (float) $hs->ppn_tarif,
+            'ppnbm_tarif' => (float) $hs->ppnbm_tarif,
             'ppn_text' => $hs->ppn_text,
             'ppnbm_text' => $hs->ppnbm_text,
             'satuan_spesifik' => $hs->satuan_spesifik
