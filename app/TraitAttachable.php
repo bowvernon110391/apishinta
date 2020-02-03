10f912dd80cc48faf686eedb5e06b6ac9848bdea
Bow Vernon <bowvernon110391@gmail.com>
2020-02-03 18:23:57 +0700

    Add method to TraitAttachable to instantiate all attachments

--- app/TraitAttachable.php
+++ app/TraitAttachable.php
@@ -6,4 +6,16 @@ trait TraitAttachable {
     public function lampiran() {
         return $this->morphMany('App\Lampiran', 'attachable');
     }
+
+    // instantiate all lampiran on disk
+    public function instantiateAllLampiran() {
+        $lampiran = $this->lampiran;
+
+        $cnt = $lampiran->count();
+        foreach ($lampiran as $l) {
+            if (!$l->instantiateOnDisk()) $cnt--;
+        }
+
+        return $cnt;
+    }
 }
