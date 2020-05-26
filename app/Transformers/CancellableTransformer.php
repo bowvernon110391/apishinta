1eb984b1fa607a585d3ccc44bcbe2625daed684f
PDAD Soetta <pdad.soetta@gmail.com>
2020-05-26 16:07:02 +0700

    CancellableTransformer shows dokumen nomor_lengkap and tgl_dok

--- app/Transformers/CancellableTransformer.php
+++ app/Transformers/CancellableTransformer.php
@@ -7,10 +7,20 @@ use League\Fractal\TransformerAbstract;
 
 class CancellableTransformer extends TransformerAbstract {
     public function transform(Cancellable $c) {
+        $data  = $c->instance;
+        $detail = [];
+
+        if ($data) {
+            $detail['nomor_lengkap']    = $data->nomor_lengkap_dok;
+            $detail['tgl_dok']          = $data->tgl_dok;
+        }
+
         return [
             'id'    => (int) $c->id,
             'cancellable_type'  => $c->cancellable_type,
             'cancellable_id'    => $c->cancellable_id,
+
+            'detail'            => $detail,
 
             'created_at'        => (string) $c->created_at,
             'updated_at'        => (string) $c->updated_at
